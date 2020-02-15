8ff707f0f2e6f3e53b985a325b6158d9212d6293
Danilo Freitas <danilofs.ti@gmail.com>
2020-02-15 18:07:48 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/danilo/cursomc/resources/ClienteResource.java
@@ -0,0 +1,27 @@
+package com.danilo.cursomc.resources;
+
+import com.danilo.cursomc.domain.Cliente;
+import com.danilo.cursomc.services.ClienteService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+
+    @Autowired
+    private ClienteService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET) 
+    public ResponseEntity<?> find(@PathVariable final Integer id){
+
+        final Cliente obj = service.buscar(id);
+        //Retorna uma resposta de sucesso com o conteudo sendo o objeto encontrado
+        return ResponseEntity.ok().body(obj);
+    }
+}
