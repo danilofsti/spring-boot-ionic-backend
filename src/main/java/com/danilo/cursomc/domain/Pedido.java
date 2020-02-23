fb0af3fad21dbf6d715e902fe349b610b86702d8
Danilo Freitas <danilofs.ti@gmail.com>
2020-02-23 18:23:37 -0300

    Subtotal e total do Pedido

--- src/main/java/com/danilo/cursomc/domain/Pedido.java
+++ src/main/java/com/danilo/cursomc/domain/Pedido.java
@@ -112,6 +112,14 @@ public class Pedido implements Serializable{
         this.itens = itens;
     }
     
+    public double getValorTotal(){
+        double soma = 0.0;
+        for(ItemPedido item: this.itens){
+            soma+= item.getSubTotal();
+        }
+        return soma;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
