7b7b4f19e41ceeff4206103532c35ce272fa5c89
Danilo Freitas <danilofs.ti@gmail.com>
2020-02-23 12:39:41 -0300

    ClienteDTO

---
+++ src/main/java/com/danilo/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,61 @@
+package com.danilo.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import com.danilo.cursomc.domain.Cliente;
+
+import org.hibernate.validator.constraints.Length;
+
+public class ClienteDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Length(min = 5, max = 120, message = "O tamanho deve conter entre 5 e 120 caracteres")
+    private String nome;
+    @Email(message = "Email inválido")
+    @NotEmpty(message = "Preenchimento obrigatório")
+    private String email;
+
+    public ClienteDTO() {
+    }
+
+    public ClienteDTO(Cliente obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.email = obj.getEmail();
+    }
+
+    public ClienteDTO(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
